349c0264849ef4b954014428a63962063f962568
yulianacmoreno <yuliana.moreno@udea.edu.co>
2018-03-17 12:02:20 -0500

    Creación del proyecto

---
+++ humanware/humanware/humanware/Controllers/ofertadeempleoController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace humanware.Controllers
+{
+    public class ofertadeempleoController : Controller
+    {
+        //
+        // GET: /ofertadeempleo/
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //
+        // GET: /ofertadeempleo/Crear
+        public ActionResult Crear()
+        {
+            return View();
+        }
+	}
+}
---
+++ humanware/humanware/humanware/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(humanware.Startup))]
+namespace humanware
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
